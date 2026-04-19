a0be6322a9a3af163f4c40a9119400f387f06fa3
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2026-04-19 16:21:03 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+         Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> emap : map.entrySet()) {
+            int value = emap.getValue();
+            if (value > 1)
+                return true;
+        }
+        return false;
+    }
+}
